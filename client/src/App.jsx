b397285b822154a69e5c10330c25c8aaab8f88e9
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-11-06 20:10:32 +0200

    Fix userData format

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,9 +22,28 @@ function App() {
 
         const formData = new FormData(event.target);
 
-        const userData = Object.fromEntries(formData);
+        const { country, city, street, streetNumber, ...userData } = Object.fromEntries(formData);
+        userData.address = {
+            country,
+            city,
+            street,
+            streetNumber,
+        };
 
-        console.log(userData);
+        userData.createdAt = new Date().toISOString();
+        userData.createdAt = new Date().toISOString();
+
+        fetch('http://localhost:3030/jsonstore/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(userData)
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result);
+            })
     }
 
     return (
